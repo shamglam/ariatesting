626bbb3754c6a8e59f44e75214fcc454f4d380c0
shamglam <shampo@hotmail.com>
2025-11-09 21:25:14 +0100

    more es lint stuff

--- src/ariaRoles/Tested/Banner.tsx
+++ src/ariaRoles/Tested/Banner.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-import { ImgRole } from '../ImgRole';
 export const Banner = () => <div role="banner">
     <h4>Velkommen til nettsteder, dette er et banner område til 
         det leses som "banner"-landemerke </h4>
